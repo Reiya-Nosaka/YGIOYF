2b494e72319fc5cea942f2c481fe0c5a58267203
tsunor <50665435+tsunor@users.noreply.github.com>
2019-05-29 15:06:34 +0900

    Assets

---
+++ Assets/DestroySelf.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySelf : MonoBehaviour {
+
+	public float Delay = 3f;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, Delay);
+	}
+}
---
+++ Assets/bomberd.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bomberd : MonoBehaviour {
+	public bool destroyed = false;//壊れたらtrue
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("Explosion"))
+		{
+			Debug.Log ("The wall destroyed!");
+			destroyed = true;
+			Destroy (gameObject);
+		}
+	}
+}
